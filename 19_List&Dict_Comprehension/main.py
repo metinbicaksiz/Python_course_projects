25fb3c6251100f88182a486198bad7aebec52f5b
metinbicaksiz <metinbicaksiz@gmail.com>
2025-05-29 21:32:39 -0700

    list comperehension

---
+++ 19_List&Dict_Comprehension/main.py
@@ -0,0 +1,20 @@
+# list = [1, 2, 3, 4, 5]
+# new_list = [ item * 2 for item in list]
+
+# name = "Metin"
+# letters = [letter for letter in name]
+#
+# numbersInRange = [number for number in range(10)]
+#
+# myTuple = (1, 2, 3, 4, 5)
+# listFromTuple = [number for number in myTuple]
+# listFromTupleMoreThan3 = [number for number in myTuple if number > 3]
+
+# names = ["Metin", "Kate", "Dafne", "Snezhana", "Nuriye"]
+# ages = [42, 38, 7, 70, 72]
+# agesDict = {name:age for name, age in zip(names, ages)}
+# young_ones = {name:age for name, age in agesDict.items() if age < 40}
+
+# sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# list = [word for word in sentence.split()]
+# result = {key:len(key) for key in list}
